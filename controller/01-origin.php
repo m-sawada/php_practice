ea8300c41b25b7756edf8cc7865826a277b73fcd
sawadamizuho <m-sawada@biglobe.co.jp>
2015-06-29 11:15:01 +0900

    controllerの01-origin.phpを追加

---
+++ controller/01-origin.php
@@ -0,0 +1,25 @@
+<?php
+
+private function externalContractRefer($model)
+{
+    $displayService = [];
+    if ($model->hasLteData()) {
+        array_push($displayService, 'LTE');
+    }
+    if ($model->has3g()) {
+        array_push($displayService, '3G');
+    }
+    if ($model->hasEmobile()) {
+        array_push($displayService, 'Emobile');
+    }
+    if ($model->hasWiMAX2()) {
+        array_push($displayService, 'WiMAX2');
+    }
+    if ($model->hasWiMAXau()) {
+        array_push($displayService, 'WiMAXau');
+    }
+    if ($model->hasWiMAX()) {
+        array_push($displayService, 'WiMAX');
+    }
+    return $displayService;
+}
